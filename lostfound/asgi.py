bd296e808daf8608a5a1c9181890c2a6ac96b578
Dhanunjaya18 <dhanunjaya0340@gmail.com>
2026-02-25 19:23:49 +0530

    Fixed ASGI app registry loading issue

--- lostfound/asgi.py
+++ lostfound/asgi.py
@@ -1,10 +1,15 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import messaging.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lostfound.settings')
+
+django.setup()   # 🔥 VERY IMPORTANT
+
+import messaging.routing  # Import AFTER setup
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
